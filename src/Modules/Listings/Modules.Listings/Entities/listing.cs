c7bfe362e251bea1f18c72f772087db6c15b3160
Marko Misimovic <misimovicmarko@gmail.com>
2024-12-13 10:08:29 +0100

    Add listing entity

---
+++ src/Modules/Listings/Modules.Listings/Entities/listing.cs
@@ -0,0 +1,13 @@
+﻿namespace Modules.Listings.Entities;
+
+public class Listing
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public string ImageUrl { get; set; } = default!;
+    public string AccommodationType { get; set; } = default!;
+    public string MainLocation { get; set; } = default!;
+    public string SubLocation { get; set; } = default!;
+    public decimal Price { get; set; }
+}
