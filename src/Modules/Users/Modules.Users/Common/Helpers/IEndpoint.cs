0a92837ab06a95953234fbbe3f188b1fd7e7dfed
MisimoM <139159816+MisimoM@users.noreply.github.com>
2024-12-16 12:22:48 +0100

    Revert "Feature/listing search/filter"

---
+++ src/Modules/Users/Modules.Users/Common/Helpers/IEndpoint.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+
+namespace Modules.Users.Common.Helpers;
+
+public interface IEndpoint
+{
+    void MapEndpoint(IEndpointRouteBuilder builder);
+}
--- src/Shared/IEndpoint.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Routing;
-
-namespace Shared;
-
-public interface IEndpoint
-{
-    void MapEndpoint(IEndpointRouteBuilder builder);
-}
