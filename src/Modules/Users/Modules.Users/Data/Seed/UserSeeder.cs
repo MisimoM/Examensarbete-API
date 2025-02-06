3c248fef8f46d6f592d11dc8ac30e693b01d2206
Marko Misimovic <misimovicmarko@gmail.com>
2025-02-06 09:41:51 +0100

    Updated seeding

--- src/Modules/Users/Modules.Users/Data/Seed/UserSeeder.cs
+++ src/Modules/Users/Modules.Users/Data/Seed/UserSeeder.cs
@@ -18,16 +18,43 @@ internal class UserSeeder
                 passwordHasher.Hash("Admin123")
             );
 
-            var hostUser = new User
+            var user1 = new User
             (
                 Guid.Parse("d3b07384-d9a9-4e6b-a0b9-2f084bc16961"),
-                "Host User",
-                "host@host.com",
+                "Conny Svensson",
+                "conny@email.com",
                 UserRole.Host.ToString(),
-                passwordHasher.Hash("Host123")
+                passwordHasher.Hash("Conny123")
             );
 
-            dbContext.Users.AddRange(adminUser, hostUser);
+           var user2 = new User
+           (
+               Guid.Parse("f2a7e6b3-8c1d-4c5d-a1f6-9e3d5e7b8a2c"),
+               "Lars Andersson",
+               "lars@email.com",
+               UserRole.Host.ToString(),
+               passwordHasher.Hash("Lars123")
+           );
+
+           var user3 = new User
+           (
+               Guid.Parse("d9f4b8e2-3a7c-4d5f-8b1e-6c2a3e9d7f5b"),
+               "Lena Larsson",
+               "lena@email.com",
+               UserRole.Host.ToString(),
+               passwordHasher.Hash("Lena123")
+           );
+
+           var user4 = new User
+           (
+               Guid.Parse("a1c5e7d3-9b2f-4d8c-6f3a-7e5b1d2f9c8a"),
+               "Karin Persson",
+               "karin@email.com",
+               UserRole.Host.ToString(),
+               passwordHasher.Hash("Karin123")
+           );
+
+            dbContext.Users.AddRange(adminUser, user1, user2, user3, user4);
             dbContext.SaveChanges();
         }
     }
